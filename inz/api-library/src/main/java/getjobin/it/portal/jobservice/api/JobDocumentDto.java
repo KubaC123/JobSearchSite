fc94962732a1c61e7322839d93c39a1f3a1f34a5
KubaC123 <pikachucichy@gmail.com>
2019-10-13 12:08:24 +0200

    + refactor for job document

--- inz/api-library/src/main/java/getjobin/it/portal/jobservice/api/JobDocumentDto.java
+++ inz/api-library/src/main/java/getjobin/it/portal/jobservice/api/JobDocumentDto.java
@@ -15,29 +15,18 @@ public class JobDocumentDto {
     private Long id;
     private String type;
     private String title;
-    private Long companyId;
     private String companyName;
-    private Long categoryId;
+    private String companyDescription;
     private String categoryName;
     private String experienceLevel;
     private String employmentType;
-    private Integer salaryMin;
-    private Integer salaryMax;
-    private Boolean flexibleWorkHours;
+    private String salaryMin;
+    private String salaryMax;
     private String description;
-    private Long technologyId;
     private String technologyName;
     private String projectDescription;
-    private Integer development;
-    private Integer testing;
-    private Integer maintenance;
-    private Integer clientSupport;
-    private Integer meetings;
-    private Integer leading;
-    private Integer documentation;
-    private Integer otherActivities;
     private Boolean active;
-    private List<JobTechStackDocumentDto> techStacks;
+    private String techStacks;
 
     @JsonPOJOBuilder(withPrefix = "")
     public static class JobDocumentDtoBuilder { }
